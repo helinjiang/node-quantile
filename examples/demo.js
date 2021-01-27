3038801877ddec93b02192a6d5314618c8acda7e
linjianghe <linjianghe@tencent.com>
2021-01-27 14:18:26 +0800

    feat: demo

--- examples/demo.js
+++ examples/demo.js
@@ -2,7 +2,5 @@ const Quantile = require('../').default;
 
 const quantile = new Quantile([6, 7, 15, 36, 39, 40, 41, 42, 43, 47, 49]);
 
-
-
-console.log(quantile.getPosition(1));
-console.log(quantile.figure(1));
+console.log(quantile.getPosition(0.25));
+console.log(quantile.figure(0.25));
